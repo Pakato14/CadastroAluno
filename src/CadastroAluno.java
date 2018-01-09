b2c525157fb4f85ecd1df8c2d0391735401f069f
Pakato14 <danieloliveira14@gmail.com>
2018-01-09 13:14:40 -0300

    Reiniciando o projeto Cadastro alunos depois de uma burrada

---
+++ src/CadastroAluno.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class CadastroAluno extends JFrame {
+	
+	public CadastroAluno() {
+		setTitle("Cadastro de Aluno");
+		setBounds(100, 100, 500, 350);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
